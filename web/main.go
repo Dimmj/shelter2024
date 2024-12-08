b9283fc73a84633e1d8e77d747d30dbd6d8ae142
Dimmj <118057923+Dimmj@users.noreply.github.com>
2024-12-08 23:33:26 +0300

    Add files via upload
    Теперь при регистрации пользователя данные заносятся в базу данных, а при входе (проверка на авторизацию еще не реализована) пользователь переадресовывается на другую страницу

--- web/main.go
+++ web/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strconv"
+
+	_ "github.com/ncruces/go-sqlite3/driver"
+	_ "github.com/ncruces/go-sqlite3/embed"
 )
+
+type user struct {
+	name     string
+	email    string
+	password string
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -33,6 +43,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm_error")
+			return
+		}
+		for key, values := range r.Form {
+			fmt.Printf("%s: %v\n", key, values)
+		}
+	}
 }
 
 func admin(w http.ResponseWriter, r *http.Request) {
@@ -48,8 +68,73 @@ func advert(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Объявление с id", id)
 }
 
+func enter(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("../ui/html/enter.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	if r.Method == http.MethodGet {
+		log.Println("geeet")
+		err = ts.Execute(w, nil)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", 500)
+		}
+	}
+	if r.Method == http.MethodPost {
+		log.Println("poost")
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm_error")
+			return
+		}
+		//formData := make(map[string][]string)
+		for key, values := range r.Form {
+			//formData[key] = values
+			fmt.Printf("%s: %v\n", key, values)
+		}
+		targetURL := "/admin"
+		http.Redirect(w, r, targetURL, http.StatusFound)
+	}
+}
+
 func registration(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Регистрация в разработке"))
+	ts, err := template.ParseFiles("../ui/html/regist.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm_error")
+			return
+		}
+		formData := make(map[string]string)
+		for key, values := range r.Form {
+			formData[key] = values[0]
+			fmt.Printf("%s: %v\n", key, values)
+		}
+
+		db, err := sql.Open("sqlite3", "../ui/static/All_db.db")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+		fmt.Println("insert into log_pswd (login, password, id_level, fio, tel) values ('" + formData["name"] + "', '" + formData["password"] + "', 1, NULL, NULL)")
+		_, err = db.Exec("insert into log_pswd (login, password, id_level, fio, tel) values ('" + formData["name"] + "', '" + formData["password"] + "', 1, NULL, NULL)")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
@@ -58,6 +143,7 @@ func main() {
 	mux.HandleFunc("/admin", admin)
 	mux.HandleFunc("/admin/advert", advert)
 	mux.HandleFunc("/registration", registration)
+	mux.HandleFunc("/enter", enter)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
